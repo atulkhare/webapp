d1c9ca27868a8052d5fd7ffccdbd7fead1ffea05
atulkhare <atul.khare01@gmail.com>
2021-06-16 11:20:43 +0530

    initial commit

---
+++ src/main/java/com/aws/deployment/DemoAwsDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.aws.deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/deployment/DemoAwsDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAwsDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

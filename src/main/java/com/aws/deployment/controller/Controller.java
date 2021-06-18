5acb19a56fa7fc5d5e5a0ffc06f96d320f6ae6b5
atulkhare <atul.khare01@gmail.com>
2021-06-18 15:10:24 +0530

    aws deployment fix 3

--- src/main/java/com/aws/deployment/controller/Controller.java
+++ src/main/java/com/aws/deployment/controller/Controller.java
@@ -1,14 +1,10 @@
 package com.aws.deployment.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+//@RestController
 public class Controller {
 
-	@GetMapping("/test/{value}")
-	private String getTest(@PathVariable("value") String value) {
-		return value;
-	}
+//	@GetMapping("/test/{value}")
+//	private String getTest(@PathVariable("value") String value) {
+//		return value;
+//	}
 }

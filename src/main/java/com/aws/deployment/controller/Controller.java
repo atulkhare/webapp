a5f5cee17131a8f2c319bc4b833ad242e36f8c25
atulkhare <atul.khare01@gmail.com>
2021-06-19 14:38:18 +0530

    aws deployment fix 2

--- src/main/java/com/aws/deployment/controller/Controller.java
+++ src/main/java/com/aws/deployment/controller/Controller.java
@@ -1,14 +1,13 @@
 package com.aws.deployment.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-	@GetMapping("/test/{value}")
-	private String getTest(@PathVariable("value") String value) {
-		return value;
+	@GetMapping("/home")
+	private String getTest() {
+		return "Rajput Online Classes Project is under development, please be patient.";
 	}
 }
